cdf760ab35cd0d98abcb7957524bf3043cee3e7c
atmanand, sah <atmanand17@gmail.com>
2020-02-02 14:32:24 +0530

    rishavcheck

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,28 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-
-
-
-
 var app = express();
 
-app.post('/',function(req,res){
-    console.log('requested');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
 
+
+
+
+app.get("/", (req, res)=>{
+    res.render ("/frontend/admin.html");
+})
+app.post('/submit',function(req,res){
+    console.log('requested');
+    res.send()
 });
+
+
+
+mongoose.connect('mongodb://localhost/einn',{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>console.log("database connected"));
+
+app.listen(5000, ()=>{
+    console.log("app started");
+})
--- frontend/admin.js
+++
@@ -1,9 +0,0 @@
-
-var final  = document.getElementById('formsubmit');
-final.addEventListener('submit',submitto);
-function submitto(data){
-    // e.prventDefault();
-    // var le = document.getElementById('nameform').value;
-    // console.log(le);
-    console.log(data);
-}
